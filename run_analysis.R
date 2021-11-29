e3ff79a891d72ea06b3175f324d1b133ad017c6a
Nuno <nunomartins_11@hotmail.com>
2021-11-29 11:47:21 +0000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(data.table)
 
-##download data
+# download data
 if(!file.exists("prog_assing_week4_data.zip")){
         fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         download.file(fileUrl,destfile="prog_assing_week4_data.zip",method="curl")
@@ -10,13 +10,13 @@ if(!file.exists("prog_assing_week4_data.zip")){
 if (!file.exists("UCI HAR Dataset")) { 
         unzip('prog_assing_week4_data.zip') 
 }
-##colnames of features
+# colnames of features
 features <- read.table("UCI HAR Dataset/features.txt")
 
-#proper labels of activities
+# proper labels of activities
 activities <- read.table("UCI HAR Dataset/activity_labels.txt")
 
-##Merges the training and the test sets to create one data set.
+# Merges the training and the test sets to create one data set.
 train_test_data=list("a","b","c")
 filenames<-list.files('./UCI HAR Dataset/train',pattern='.txt$')
 names(train_test_data)<-sub("_.*", "", filenames)
@@ -35,18 +35,18 @@ colnames(train_test_data$y)[1]<-'y'
 
 merged_data<-cbind(train_test_data$subject,train_test_data$X,train_test_data$y)
 
-##xtracts only the measurements on the mean and standard deviation for each measurement. 
+# extracts only the measurements on the mean and standard deviation for each measurement. 
 tidy_data<-merged_data %>% select(Subject,y,contains("mean"),contains("std"))
 ##atribute the readble classes to activities
 levels(tidy_data$y)<-activities$V2
 colnames(tidy_data)[2] <- 'Activity'
 
-##aggregated tidy data set with the average of each variable for each activity and each subject.
+# aggregated tidy data set with the average of each variable for each activity and each subject.
 tidy_data_agg_mean<-tidy_data %>% 
         group_by(Subject,Activity) %>%
         summarise_all(mean,na.rm=T)
 
-##more readable column names
+# more readable column names
 names(tidy_data_agg_mean)<-gsub("-", "_", names(tidy_data_agg_mean))
 names(tidy_data_agg_mean)<-gsub("\\(", "", names(tidy_data_agg_mean))
 names(tidy_data_agg_mean)<-gsub("\\)", "", names(tidy_data_agg_mean))
